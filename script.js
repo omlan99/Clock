5d21b72fd352c6352e38c485dcf3a800b6559bca
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-08-29 01:16:22 +0600

    code

---
+++ script.js
@@ -0,0 +1,19 @@
+let hr = document.getElementById("hour")
+let min = document.getElementById("minute")
+let sec = document.getElementById("second")
+
+function displayTime(){
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+     
+    let hrotate = 30*hh + mm/2;
+    let mrotate = 6*mm;
+    let srotate = 6*ss;
+    hr.style.transform =`rotate(${hrotate}deg)`;
+    min.style.transform =`rotate(${mrotate}deg)`;
+    sec.style.transform =`rotate(${srotate}deg)`;
+}
+
+    setInterval(displayTime, 1000);
